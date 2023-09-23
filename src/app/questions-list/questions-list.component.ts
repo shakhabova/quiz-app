a1933479593438efcd7f5e34c5458b59dc42487f
Алина Шахабова <alina.shakhabova@yandex.ru>
2023-09-23 15:08:29 +0300

    questions service

--- src/app/questions-list/questions-list.component.ts
+++ src/app/questions-list/questions-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Question } from '../Question';
 import { Router } from '@angular/router';
+import { QuestionsService } from '../questions.service';
 
 @Component({
   selector: 'app-questions-list',
@@ -10,43 +11,10 @@ import { Router } from '@angular/router';
 export class QuestionsListComponent {
   title = 'quiz-app';
   public rightAnswersCount = 0;
+  public questions = this.questionsService.questions;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private questionsService: QuestionsService) {}
 
-  public questions: Question[] = [
-    {
-      id: 1,
-      description: 'Your favorite season is',
-      multiple_correct_answers: true,
-      answers: {
-        answer_a: 'Summer',
-        answer_b: 'Winter',
-        answer_c: 'Spring',
-        answer_d: 'Autumn',
-      },
-      correct_answers: {
-        answer_a_correct: false,
-        answer_b_correct: false,
-        answer_c_correct: true,
-        answer_d_correct: false,
-      },
-    },
-    {
-      id: 2,
-      description: 'Your favorite food is',
-      multiple_correct_answers: false,
-      answers: {
-        answer_a: 'Pizza',
-        answer_b: 'Soup',
-        answer_c: 'Meat',
-      },
-      correct_answers: {
-        answer_a_correct: true,
-        answer_b_correct: false,
-        answer_c_correct: false,
-      },
-    },
-  ];
 
   public onAnswer(isRight: boolean) {
     if (isRight) {
---
+++ src/app/questions.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { Question } from './Question';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionsService {
+
+  
+  public questions: Question[] = [
+    {
+      id: 1,
+      description: 'It1 a great movie. I have ... tha movie many times',
+      multiple_correct_answers: true,
+      answers: {
+        answer_a: 'saw',
+        answer_b: 'seen',
+        answer_c: 'see',
+        answer_d: 'sees',
+      },
+      correct_answers: {
+        answer_a_correct: false,
+        answer_b_correct: true,
+        answer_c_correct: false,
+        answer_d_correct: false,
+      },
+    },
+    {
+      id: 2,
+      description: 'Have you ... really strange or interesting food',
+      multiple_correct_answers: false,
+      answers: {
+        answer_a: 'ever eaten',
+        answer_b: 'never eaten',
+        answer_c: 'ate',
+      },
+      correct_answers: {
+        answer_a_correct: true,
+        answer_b_correct: false,
+        answer_c_correct: false,
+      },
+    },
+    {
+      id: 3,
+      description: '... your brother talked to you yet? Yes, he ...',
+      multiple_correct_answers: false,
+      answers: {
+        answer_a: 'Has/did',
+        answer_b: 'Have/have',
+        answer_c: 'Has/has',
+      },
+      correct_answers: {
+        answer_a_correct: false,
+        answer_b_correct: false,
+        answer_c_correct: true,
+      },
+    },
+    
+  ];
+  constructor() { }
+}
