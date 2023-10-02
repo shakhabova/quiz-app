42fa0e20af6dc0f58b763e34c42b02cc7c880f4d
Алина Шахабова <alina.shakhabova@yandex.ru>
2023-10-02 13:50:20 +0300

    result component update

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,14 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
 import { AnswerNames, CorrectAnswers, Question } from '../Question';
 import { Result } from '../test-resuts.model';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class QuestionComponent {
   @Input() public question!: Question;
+  @Input() public result?: Result;
   @Output() public answered = new EventEmitter<Result>();
 
   onChange(answer: AnswerNames) {
@@ -19,4 +22,8 @@ export class QuestionComponent {
       isRight: this.question.correct_answers[correctKey] ?? false,
     });
   }
+
+  // public getRadioButtonColor(answerName: AnswerNames): ThemePalette {
+
+  // }
 }
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AnswersService } from '../answers.service';
+import { Result } from '../test-resuts.model';
+import { QuestionsService } from '../questions.service';
+import { Question } from '../Question';
 
 @Component({
   selector: 'app-results',
@@ -11,12 +14,15 @@ export class ResultsComponent implements OnInit {
   public value!: number;
   public rightAnswersCount!: number;
   public questionsLength!: number;
+  public results: Result[] = [];
+  public questions: Question[] = [];
 
   private testResultsId!: number;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private answersService: AnswersService
+    private answersService: AnswersService,
+    private questionsService: QuestionsService
   ) {}
 
   ngOnInit() {
@@ -30,11 +36,22 @@ export class ResultsComponent implements OnInit {
     if (!testResults) {
       throw new Error('test result not found');
     }
+    
+    const test = this.questionsService.getTest(testResults.testId);
+    if (!test) {
+      throw new Error('test is not found');
+    }
+    this.questions = test.questions;
+  
+    this.results = testResults.results;
 
     this.rightAnswersCount = testResults.results.filter(
       (res) => res.isRight
     ).length;
     this.questionsLength = testResults.results.length;
     this.value = (this.rightAnswersCount / this.questionsLength) * 100;
+  }
+  getResult(questionId: number): Result {
+    return this.results.find(result => result.questionId === questionId)!;
   }
 }
